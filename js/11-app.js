b1da82713e921ecbc67bf3cff804d61f285962b7
ph4ntomkn1ght <jrescam@gmail.com>
2021-08-17 15:40:45 -0500

    Agregamos el nuevo archivo 11-app.js

---
+++ js/11-app.js
@@ -0,0 +1,9 @@
+/*funciones sin poner nada*/
+
+function nueva1Funcion(){
+    console.log("Hola a todos desde la rama login_3");
+}//nueva1Funcion
+
+function appFuncion(){
+    console.log("hola a todos desde la modificacion");
+}//appFuncion()
